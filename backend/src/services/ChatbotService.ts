e02ca309942f1fdb53451962a0b8e47dfc77251d
Yevgeny Kyselov <eukiselevua@gmail.com>
2020-05-24 13:57:06 +0300

    backup

--- backend/src/services/ChatbotService.ts
+++ backend/src/services/ChatbotService.ts
@@ -3,61 +3,73 @@ import _ from 'lodash';
 
 class ChatbotService {
 
-  private client;
+    private client;
 
-  constructor() {
-    this.client = new elastic.Client({
-      host: 'localhost:9200',
-      log: 'trace',
-      apiVersion: '7.2'
-    });
-  }
-
-  async addQuestion(data) {
-    return await this.client.create({
-      index: 'chat',
-      type: 'question',
-      id: Math.random(),
-      body: data
-    });
-  }
-
-  async findAnswers(question) {
-    const data = await this.client.search({
-      index: 'chat',
-      type: 'question',
-      size: 10,
-      body: {
-        query: {
-          bool: {
-            must: [{
-              query_string: {
-                query: `*${question}*`,
-                fields: [
-                  'question'
-                ],
-              },
-            },
-            { exists: { field: 'answer' }}
-            ],
-          }
-        }
-      }
-    });
-
-    return data;
-  }
-
-  async findRandomAnswer(answers) {
-    let randomAnswer;
-
-    if (_.get(answers, 'hits.total.value', 0) > 0) {
-      const ansArr = _.get(answers, 'hits.hits', []);
-      randomAnswer = ansArr[Math.floor(Math.random() * ansArr.length)];
+    constructor() {
+        this.client = new elastic.Client({
+            host: 'localhost:9200',
+            log: 'trace',
+            apiVersion: '7.2'
+        });
     }
 
-    return randomAnswer;
-  }
+    async addQuestion(data) {
+        try {
+            return await this.client.create({
+                index: 'chat',
+                type: 'question',
+                id: Math.random(),
+                body: data
+            });
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async findAnswers(question) {
+        try {
+            const data = await this.client.search({
+                index: 'chat',
+                type: 'question',
+                size: 10,
+                body: {
+                    query: {
+                        bool: {
+                            must: [{
+                                query_string: {
+                                    query: `*${question}*`,
+                                    fields: [
+                                        'question'
+                                    ],
+                                },
+                            },
+                                {exists: {field: 'answer'}}
+                            ],
+                        }
+                    }
+                }
+            });
+
+            return data;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    async findRandomAnswer(answers) {
+        try {
+            let randomAnswer;
+
+            if (_.get(answers, 'hits.total.value', 0) > 0) {
+                const ansArr = _.get(answers, 'hits.hits', []);
+                randomAnswer = ansArr[Math.floor(Math.random() * ansArr.length)];
+            }
+
+            return randomAnswer;
+        } catch (e) {
+            throw e;
+        }
+    }
 }
 
 export default ChatbotService;
